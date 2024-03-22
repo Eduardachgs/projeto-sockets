a96c7709208fa7163f73c16e4e99ae21de5f95ad
Eduardachagas <dudachagasf@outlook.com>
2024-03-22 07:24:13 -0300

    implementacao de chaves no pc

--- autoridade.py
+++ autoridade.py
@@ -35,31 +35,6 @@ def process_request(server_socket):
 server_socket = socket(AF_INET, SOCK_DGRAM)
 server_socket.bind(("localhost", 7777))
 
-# autoridade irá atender 6 PCs
-for i in range(6):
-    Thread(target=process_request, args=(server_socket,)).start()
-
-
-def client_function(destino):
-    # criar socket UDP
-    socket_client = socket(AF_INET, SOCK_DGRAM)
-    server_address = ('localhost', destino)
-
-    # loop para que o cliente envie até 3 solicitações
-    for i in range(3):
-        message = input(">>>")
-
-        # enviamos a mensagem pelo socket criado
-        socket_client.sendto(message.encode(), server_address)
-
-        # recebendo as respostas do servidor
-        ans, sAddress = socket_client.recvfrom(2048)
-        ans = ans.decode()
-
-        print(f"A resposta recebida foi: {ans}")
-
-    socket_client.close()
-
 
 ca_thread = Thread(target=process_request(server_socket), args=(7777,))
 ca_thread.start()
--- pc.py
+++ pc.py
@@ -1,14 +1,14 @@
 from roteamento import Roteamento
 from socket import socket, AF_INET, SOCK_DGRAM
 import rsa
-import pickle
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 roteamento = Roteamento()
 class Pc:
     def __init__(self, pc, porta):
         self.pc = pc
         self.porta = porta
-        self.simetric_keys = {} # dicionário para armazenar as chaves simétricas
         self.privKey = None
         self.client_socket = socket(AF_INET, SOCK_DGRAM)
         self.server_socket = socket(AF_INET, SOCK_DGRAM)
@@ -16,13 +16,12 @@ class Pc:
     # função para que o nó possa se registrar na autoridade
     def register(self):
         server_address = ('localhost', 7777) # endereço e porta da autoridade
-        request = 'register'
+        request = f'{self.pc}:register'
         self.client_socket.sendto(request.encode(), server_address)
         
         print('Registrando o nó na autoridade...')
         ans, sAddress = self.client_socket.recvfrom(2048)
-        # ans = pickle.loads(ans)
-        # self.privKey = rsa.decrypt(ans, privkey)
+        self.privKey = rsa.PrivateKey.load_pkcs1(ans)
         
         print(f'Nó registrado com sucesso!')
 
@@ -34,12 +33,49 @@ class Pc:
         # loop para receber mensagens dos outros PCs
         while True:
             message, clientAddress = self.server_socket.recvfrom(2048) # recebe até 2048 bytes
-            request = message.decode()
+            try:
+                request = message.decode()
+            except:
+                request = message
+            
+            if request[:-1] == 'keys':
+                print()
+                # ARRUMAR ISSO AQUI
+                key, clientAddress = self.server_socket.recvfrom(2048)
+                nonce, clientAddress = self.server_socket.recvfrom(2048)
+                key = rsa.decrypt(key, self.privKey)
+                nonce = rsa.decrypt(nonce, self.privKey)
+                cipher = AES.new(key, AES.MODE_EAX, nonce)
+                print(f'Mensagens de PC{request[-1]}:')
+                for i in range(5):
+                    if i > 0:
+                        key, clientAddress = self.server_socket.recvfrom(2048)
+                        nonce, clientAddress = self.server_socket.recvfrom(2048)
+                        key = rsa.decrypt(key, self.privKey)
+                        nonce = rsa.decrypt(nonce, self.privKey)
+                        cipher = AES.new(key, AES.MODE_EAX, nonce)
+                    message, clientAddress = self.server_socket.recvfrom(2048)
+                    tag = message[:16]
+                    message = message[16:]
+                    message = cipher.decrypt_and_verify(message, tag).decode('utf-8')
+                    print(f"A mensagem recebida foi: {message}")
 
-            if request[0:4] == 'RRRR':
-                _, pc_origem, pc_destino, msg = request.split(':')
+
+            elif request[0:4] == 'RRRR':
+                _, pc_origem, pc_destino, original = request.split(':')
 
                 if self.pc == int(pc_destino):
+                    # adicionar a descriptografia da mensagem
+                    print(f'Mensagens de PC{original}:')
+                    key, clientAddress = self.server_socket.recvfrom(2048)
+                    nonce, clientAddress = self.server_socket.recvfrom(2048)
+                    key = rsa.decrypt(key, self.privKey)
+                    nonce = rsa.decrypt(nonce, self.privKey)
+                    cipher = AES.new(key, AES.MODE_EAX, nonce)
+                    msg, clientAddress = self.server_socket.recvfrom(2048)
+                    tag = msg[:16]
+                    msg = msg[16:]
+                    msg = cipher.decrypt_and_verify(msg, tag).decode('utf-8')
                     print(f"A mensagem recebida foi: {msg}")
                 
                 else: 
@@ -47,12 +83,16 @@ class Pc:
                     prox_pc = roteamento.enviar_mensagens(pc_origem, int(pc_destino))
                     destino = ('localhost', int(str(prox_pc)*4))
                     self.client_socket.sendto(request.encode(), destino)
-            else:
-                print()
-                print(f"A mensagem recebida de {clientAddress} foi: {request}")
+                    for i in range(3):
+                        k, clientAddress = self.server_socket.recvfrom(2048)
+                        self.client_socket.sendto(k, destino)
 
-                ans = f"Olá, cliente {clientAddress}"
-                self.server_socket.sendto(ans.encode(), clientAddress)
+            # else:
+            #     print()
+            #     print(f"A mensagem recebida de {clientAddress} foi: {request}")
+
+            #     ans = f"Olá, cliente {clientAddress}"
+            #     self.server_socket.sendto(ans.encode(), clientAddress)
 
     def client_function(self):
         iniciar = input('Digite enter para iniciar a comunicação entre os PCs')
@@ -62,43 +102,73 @@ class Pc:
             destino = int(str(i+1)*4)
             if destino == self.porta:
                 continue
+            
+            server_address = ('localhost', 7777)
+            request = f'{self.pc}:get_pubkey:{destino}'
+            self.client_socket.sendto(request.encode(), server_address)
+            ans, sAddress = self.client_socket.recvfrom(2048)
+            pubKey_destino = rsa.PublicKey.load_pkcs1(ans)
+
+
 
             server_address = ('localhost', destino)
             if (self.pc == 1 and (i+1) == 6) or (self.pc == 6 and (i+1) == 1):
+
+
+                mensagem = f'keys{self.pc}'
+                self.client_socket.sendto(mensagem.encode(), server_address)
                 for j in range(5):
-                    message = f"PC{self.pc} aqui! Mensagem {j+1} para o PC {i+1}"
-
+                    key = get_random_bytes(16)
+                    cipher = AES.new(key, AES.MODE_EAX)
+                    nonce = cipher.nonce
+                    key = rsa.encrypt(key, pubKey_destino)
+                    nonce = rsa.encrypt(nonce, pubKey_destino)
+                    self.client_socket.sendto(key, server_address)
+                    self.client_socket.sendto(nonce, server_address)
+                    message = b"Mensagem..."
+                    ciphertext, tag = cipher.encrypt_and_digest(message)
+                    message = tag+ciphertext
                     # enviamos a mensagem pelo socket criado
-                    self.client_socket.sendto(message.encode(), server_address)
-
-                    # recebendo as respostas do servidor
-                    ans, sAddress = self.client_socket.recvfrom(2048)
-                    ans = ans.decode()
-
-                    print(f"A resposta recebida foi: {ans}")
+                    self.client_socket.sendto(message, server_address)
 
             elif (i+1) == self.pc+1 or (i+1) == self.pc-1:
             # segundo loop para enviar 5 mensagens para cada PC
+                mensagem = f'keys{self.pc}'
+                self.client_socket.sendto(mensagem.encode(), server_address)
                 for j in range(5):
-                    message = f"PC{str(self.porta)[0]} aqui! Mensagem {j+1} para o PC {i+1}"
-
+                    key = get_random_bytes(16)
+                    cipher = AES.new(key, AES.MODE_EAX)
+                    nonce = cipher.nonce
+                    key = rsa.encrypt(key, pubKey_destino)
+                    nonce = rsa.encrypt(nonce, pubKey_destino)
+                    self.client_socket.sendto(key, server_address)
+                    self.client_socket.sendto(nonce, server_address)
+                    message = b"Mensagem..."
+                    ciphertext, tag = cipher.encrypt_and_digest(message)
+                    message = tag+ciphertext
                     # enviamos a mensagem pelo socket criado
-                    self.client_socket.sendto(message.encode(), server_address)
-
-                    # recebendo as respostas do servidor
-                    ans, sAddress = self.client_socket.recvfrom(2048)
-                    ans = ans.decode()
-
-                    print(f"A resposta recebida foi: {ans}")
+                    self.client_socket.sendto(message, server_address)
 
             else:
                 for j in range(5):
                     prox_pc = roteamento.enviar_mensagens(self.pc, i+1)
                     destino = ('localhost', int(str(prox_pc)*4))
 
-                    message = f'RRRR:{self.pc}:{i+1}:PC{self.pc} aqui! Mensagem {j+1} roteada para o PC {i+1}'
+                    message = f'RRRR:{self.pc}:{i+1}:{self.pc}'
                     self.client_socket.sendto(message.encode(), destino)
 
+                    key = get_random_bytes(16)
+                    cipher = AES.new(key, AES.MODE_EAX)
+                    nonce = cipher.nonce
+                    key = rsa.encrypt(key, pubKey_destino)
+                    nonce = rsa.encrypt(nonce, pubKey_destino)
+                    self.client_socket.sendto(key, server_address)
+                    self.client_socket.sendto(nonce, server_address)
+                    message = b"Mensagem..."
+                    ciphertext, tag = cipher.encrypt_and_digest(message)
+                    message = tag+ciphertext
+                    # enviamos a mensagem pelo socket criado
+                    self.client_socket.sendto(message, server_address)
+
+
             #socket_client.close()
-
-
