8308c51857fb38547348d3973725216c84da462f
Eduardachagas <dudachagasf@outlook.com>
2024-03-22 21:08:37 -0300

    roteamento do broadcast

--- pc.py
+++ pc.py
@@ -29,12 +29,22 @@ class Pc:
             request = message.decode()
 
             # Se a mensagem iniciar com a tag RRRR ela foi roteada
-            if request[0:4] == 'RRRR':
+            if request[0:4] == 'RRRR' or request[0:4] == 'BBBB':
                 _, pc_origem, pc_destino, msg = request.split(':')
 
                 # Verifica se a mensagem é para o PC atual
                 if self.pc == int(pc_destino):
-                    print(f"A mensagem recebida foi: {msg}")
+                    if request[0:4] == 'BBBB':
+                        ans = f'{self.pc} recebeu a mensagem roteada'
+                        _, pc_origem, pc_destino, msg = request.split(':')
+                        print(f"A mensagem recebida foi: {msg}")
+                        ans = f'RRRR:{self.pc}:{int(msg[2])}:PC{self.pc} recebeu a mensagem broadcast'
+                        destino = roteamento.enviar_mensagens(self.pc, int(msg[2]))
+                        destino = ('localhost', int(str(destino)*4))
+                        self.server_socket.sendto(ans.encode(), destino)
+
+                    else:
+                        print(f"A mensagem recebida foi: {msg}")
                 
                 # Se não for, roteia a mensagem para o próximo PC
                 else: 
@@ -43,14 +53,6 @@ class Pc:
                     destino = ('localhost', int(str(prox_pc)*4))
                     self.client_socket.sendto(request.encode(), destino)
 
-            # Tag BBBB indica broadcast
-            elif request[0:4] == 'BBBB':
-                _, msg = request.split(':')
-                print(f"A mensagem recebida foi: {msg}")
-                ans = f'{self.pc} recebeu a mensagem broadcast'
-                self.server_socket.sendto(ans.encode(), clientAddress)
-
-            # Se não for roteada, a mensagem é para o PC atual
             else:
                 print(f"A mensagem recebida foi: {request}")
 
@@ -64,38 +66,31 @@ class Pc:
             if destino == self.porta:
                 continue
 
-            server_address = ('localhost', destino)
+            destino = ('localhost', destino)
             # Condicionais para determinar o destino adequado dos pacotes
             if (self.pc == 1 and (i+1) == 6) or (self.pc == 6 and (i+1) == 1):
                 # segundo loop para enviar 5 mensagens para cada PC
                 for j in range(5):
                     message = f"PC{self.pc} aqui! Mensagem {j+1} para o PC {i+1}"
-                    self.client_socket.sendto(message.encode(), server_address)
+                    self.client_socket.sendto(message.encode(), destino)
 
             elif (i+1) == self.pc+1 or (i+1) == self.pc-1:
                 for j in range(5):
                     message = f"PC{str(self.porta)[0]} aqui! Mensagem {j+1} para o PC {i+1}"
 
                     # enviamos a mensagem pelo socket criado
-                    self.client_socket.sendto(message.encode(), server_address)
+                    self.client_socket.sendto(message.encode(), destino)
+                
 
             else:
+                prox_pc = roteamento.enviar_mensagens(self.pc, i+1)
+                destino = ('localhost', int(str(prox_pc)*4))
                 for j in range(5):
                     # Usa a função de roteamento para determinar o próximo PC
-                    prox_pc = roteamento.enviar_mensagens(self.pc, i+1)
-                    destino = ('localhost', int(str(prox_pc)*4))
                     message = f'RRRR:{self.pc}:{i+1}:PC{self.pc} aqui! Mensagem {j+1} roteada para o PC {i+1}'
                     self.client_socket.sendto(message.encode(), destino)
-        
-        mensagem_broadcast = f"BBBB:PC{self.pc} aqui! Mensagem broadcast para todos os PCs"
-        # Loop para enviar mensagem broadcast para todos os PCs
-        for i in range(6):
-            if self.pc == i+1:
-                continue
-            destino = ('localhost', int(str(i+1)*4))
+                
+            mensagem_broadcast = f"BBBB:{self.pc}:{i+1}:PC{self.pc} aqui! Mensagem broadcast para todos os PCs"
             self.client_socket.sendto(mensagem_broadcast.encode(), destino)
-            ans, sAddress = self.client_socket.recvfrom(2048)
-            ans = ans.decode()
-            print(ans)
-
+        
 
